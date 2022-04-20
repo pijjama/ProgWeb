bc6ad7bb16277ae1767549c3d3545df29f07c4be
pijjama <50836265+pijjama@users.noreply.github.com>
2022-04-20 17:42:51 -0400

    prova programação web 01

---
+++ prova01/questao_04.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+function readFile(filename) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filename, function (error, data) {
+      resolve(parseInt(data));
+    });
+  });
+}
+
+async function calcularValor() {
+  let valor1 = await readFile('1.txt');
+  let valor2 = await readFile('2.txt');
+  let valor3 = await readFile('3.txt');
+  console.log(valor1 + valor2 + valor3);
+}
+
+calcularValor();
